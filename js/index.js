ca866ec314b65506f1763e498cdff45492b2c2cd
matthew-heideman <heidemanmr@yahoo.com>
2020-03-11 04:03:10 -0500

    added one event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const viewImage = document.querySelectorAll('img')
+
+Array.from(viewImage).forEach(item => { item.addEventListener('dblclick', function(event){
+    console.log(event.target)
+})
+
+})
