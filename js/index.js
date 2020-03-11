2e0aa93e0a323e5c757618d355a437e756e273cd
matthew-heideman <heidemanmr@yahoo.com>
2020-03-11 16:06:41 -0500

    added two more events

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
 // Your code goes here
 const viewImage = document.querySelectorAll('img')
 
-Array.from(viewImage).forEach(item => { item.addEventListener('dblclick', function(event){
-    console.log(event.target)
-})
+Array.from(viewImage).forEach(item => { 
+    item.style.transform = 'scale(1)';
+    item.style.transition = 'transform 2s';
+    const transform = document.createElement('div')
+    
+    item.addEventListener('dblclick', function(event){
+    
+    console.log(item);
+    });
+});
 
-})
+const logo = document.querySelector(".logo-heading");
+logo.addEventListener('mouseenter', function(event){
+    console.log(event.target)
+    logo.style.transform = 'scale(1.3)';
+});
+logo.addEventListener('mouseleave', function(event){
+    console.log(event.target)
+    logo.style.transform = 'scale(1)';
+});
